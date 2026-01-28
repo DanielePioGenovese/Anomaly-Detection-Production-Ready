a78475fd8e3faa6102d8a1bc722ce541f0fc18be
EuBa-Code <soldatino95@gmail.com>
2026-01-28 14:50:47 +0100

    Second UV libraries update & sdv code improvements

--- data/create_synthetic_sdv.py
+++ data/create_synthetic_sdv.py
@@ -2,15 +2,17 @@ import pandas as pd
 import numpy as np
 from sdv.single_table import GaussianCopulaSynthesizer
 from sdv.metadata import Metadata
+from sdv.evaluation.single_table import get_column_plot
 import os
 
 # Definizione dei percorsi
 DATA_DIR = os.path.dirname(os.path.abspath(__file__))
-INPUT_FILE = os.path.join(DATA_DIR, 'test_data.csv')
+INPUT_FILE = os.path.join(DATA_DIR, 'test_data_3.csv')
 CSV_OUTPUT = os.path.join(DATA_DIR, 'synthetic_data_sdv.csv')
 PARQUET_OUTPUT = os.path.join(DATA_DIR, 'synthetic_data_sdv.parquet')
+PLOT_OUTPUT = os.path.join(DATA_DIR, 'visual_evaluation.html')
 
-def generate_synthetic_data(num_rows=2000):      # ho impostato 2000 righe come default per non esagerare, poichè faccio anche il salvataggio in CSV 
+def generate_synthetic_data(num_rows=1000):      # ho impostato 1000 righe come default per non esagerare, poichè faccio anche il salvataggio in CSV 
     print(f"Caricamento dati da {INPUT_FILE}...")
     data = pd.read_csv(INPUT_FILE)
     
@@ -44,14 +46,27 @@ def generate_synthetic_data(num_rows=2000):      # ho impostato 2000 righe come 
     # generazione dei dati con il numero di righe specificato 
     synthetic_data = synthesizer.sample(num_rows=num_rows)
     
+    # TEST DI AUTOVALUTAZIONE PER VEDERE SE I DATI SINTETICI GENERATI SONO BUONI
+    try:
+        fig = get_column_plot(
+            real_data=data,
+            synthetic_data=synthetic_data,
+            column_name='Active_Power',
+            metadata=metadata
+        )
+        # salva il grafico in HTML
+        fig.write_html(PLOT_OUTPUT)
+        print(f"Grafico di valutazione salvato in: {PLOT_OUTPUT}")
+    except Exception as e:
+        print(f"Errore nella generazione del grafico: {e}")
+
     # salvataggio in Parquet
     synthetic_data.to_parquet(PARQUET_OUTPUT, index=False)
     
-    # salvataggio in CSV
+    # salvataggio in CSV (in questo caso solo per le prove, per vedere come si comportano i dati sintetici)
     synthetic_data.to_csv(CSV_OUTPUT, index=False)
 
-
-    print("Fatto! Generazione completata con successo.")
+    print("Generazione completata con successo.")
     return synthetic_data
 
 if __name__ == "__main__":
