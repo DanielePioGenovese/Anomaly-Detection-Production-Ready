8bbfda9c520ac9ad7a6c94ba77bdb1d953b34081
tallaritadenise2-afk <tallaritadenise2@gmail.com>
2026-02-09 19:59:28 +0100

    Feature service for the machine view

--- services/feature_store_src/feature_store/src/feature_services.py
+++ services/feature_store_src/feature_store/src/feature_services.py
@@ -1,3 +1,11 @@
-from feast import FeatureService
+from feast import FeatureService 
+# Import the FeatureService class from Feast to define features that can be served to models during training and inference
+from feature_definitions import machine_view
+# Import the machine_view FeatureView that we defined in feature_definitions.py, which contains the features related to machines
 
-from feature_definitions import 
+# Create a feature service for the machine view 
+machine_feature_service_v1 = FeatureService(
+    name="machine_feature_service_v1",
+    features=[machine_view],
+    tags={"team": "ml", "project": "predictive_maintenance", "domain":"electrical_monitoring", "version": "1.0"}  # Optional metadata tags
+)
